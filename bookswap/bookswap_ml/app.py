0c940c3ab401d8bf39ac26819714bd8d180ca731
raagapranitha <raagapraneeta@gmail.com>
2020-05-03 20:45:42 -0700

    Add img url to result

--- bookswap/bookswap_ml/app.py
+++ bookswap/bookswap_ml/app.py
@@ -5,27 +5,20 @@ from flask import Flask, escape, request, jsonify
 import numpy as np
 import pandas as pd
 import collections
-#from mpl_toolkits.mplot3d import Axes3D
 from IPython import display
-#from matplotlib import pyplot as plt
 import sklearn
 import sklearn.manifold
 import tensorflow as tf
 
 app = Flask(__name__)
+pd.set_option('display.max_rows', None)
 
-
-
-# import tensorflow.compat.v2 as tf
-# tf.disable_v2_behavior()
-
-print(tf.__version__)
-
+print("Reading data")
 users_cols = ['user_id']
-users = pd.read_csv('processed/u.user', sep='|', names=users_cols, encoding='latin-1')
+users = pd.read_csv('data/user_info', sep='|', names=users_cols, encoding='latin-1')
 
 ratings_cols = ['user_id', 'book_id', 'rating']
-ratings = pd.read_csv('processed/u.data', sep='\t', names=ratings_cols, encoding='latin-1')
+ratings = pd.read_csv('data/ratings_info', sep='\t', names=ratings_cols, encoding='latin-1')
 
 
 genre_cols = ['unknown','fiction','fantasy','romance','young-adult',
@@ -34,7 +27,16 @@ genre_cols = ['unknown','fiction','fantasy','romance','young-adult',
               'humor','chick-lit','adventure','horror']
 
 books_cols = ['book_id', 'good_book_id','author','title', "img_url"] + genre_cols
-books = pd.read_csv('processed/u.item', sep='|', names=books_cols, encoding='latin-1')
+books = pd.read_csv('data/book_info', sep='|', names=books_cols, encoding='latin-1')
+
+processed_title=[]
+for name in books['title']:
+  name = name.lower()
+  if not (name.isalnum()):
+    name = ''.join(e for e in name if e.isalnum())
+  processed_title.append(name)
+books['edited_title'] = processed_title
+  
 
 users["user_id"] = users["user_id"].apply(lambda x: str(x-1))
 books["book_id"] = books["book_id"].apply(lambda x: str(x-1))
@@ -42,66 +44,35 @@ ratings["book_id"] = ratings["book_id"].apply(lambda x: str(x-1))
 ratings["user_id"] = ratings["user_id"].apply(lambda x: str(x-1))
 genre_occurences = books[genre_cols].sum().to_dict()
 
-def mark_genres(books, genres):
-  def get_random_genre(gs):
-    active = [genre for genre, g in zip(genres, gs) if g==1]
-    if len(active) == 0:
+def select_genres(books, genres):
+  def get_genres(gn):
+    present = [genre for genre, g in zip(genres, gn) if g==1]
+    if len(present) == 0:
       return 'Other'
-    return np.random.choice(active)
-  def get_all_genres(gs):
-    active = [genre for genre, g in zip(genres, gs) if g==1]
-    if len(active) == 0:
-      return 'Other'
-    return '-'.join(active)
-  books['genre'] = [
-      get_random_genre(gs) for gs in zip(*[books[genre] for genre in genres])]
+    return '-'.join(present)
+
   books['all_genres'] = [
-      get_all_genres(gs) for gs in zip(*[books[genre] for genre in genres])]
+      get_genres(gn) for gn in zip(*[books[genre] for genre in genres])]
 
-mark_genres(books, genre_cols)
+select_genres(books, genre_cols)
 
-def split_dataframe(df, holdout_fraction=0.1):
-  """Splits a DataFrame into training and test sets.
-  Args:
-    df: a dataframe.
-    holdout_fraction: fraction of dataframe rows to use in the test set.
-  Returns:
-    train: dataframe for training
-    test: dataframe for testing
-  """
-  test = df.sample(frac=holdout_fraction, replace=False)
+def divide_train_test(df, test_fraction=0.001):
+  
+  test = df.sample(frac=test_fraction, replace=False)
   train = df[~df.index.isin(test.index)]
   return train, test
 
-#users.describe()
-
-def build_rating_sparse_tensor(ratings_df):
-  """
-  Args:
-    ratings_df: a pd.DataFrame with `user_id`, `book_id` and `rating` columns.
-  Returns:
-    a tf.SparseTensor representing the ratings matrix.
-  """
-  indices = ratings_df[['user_id', 'book_id']].values
-  values = ratings_df['rating'].values
-#     users.shape[0] = 53424
+def get_matrix_from_ratings(ratings_df):
+  
+  cols = ratings_df[['user_id', 'book_id']].values
+  rows = ratings_df['rating'].values
   return tf.SparseTensor(
-      indices=indices,
-      values=values,
+      indices=cols,
+      values=rows,
       dense_shape=[users.shape[0], books.shape[0]])
 
-def sparse_mean_square_error(sparse_ratings, user_embeddings, book_embeddings):
-  """
-  Args:
-    sparse_ratings: A SparseTensor rating matrix, of dense_shape [N, M]
-    user_embeddings: A dense Tensor U of shape [N, k] where k is the embedding
-      dimension, such that U_i is the embedding of user i.
-    book_embeddings: A dense Tensor V of shape [M, k] where k is the embedding
-      dimension, such that V_j is the embedding of book j.
-  Returns:
-    A scalar Tensor representing the MSE between the true ratings and the
-      model's predictions.
-  """
+def calculate_mean_error(sparse_ratings, user_embeddings, book_embeddings):
+  
   predictions = tf.reduce_sum(
       tf.gather(user_embeddings, sparse_ratings.indices[:, 0]) *
       tf.gather(book_embeddings, sparse_ratings.indices[:, 1]),
@@ -109,21 +80,15 @@ def sparse_mean_square_error(sparse_ratings, user_embeddings, book_embeddings):
   loss = tf.losses.mean_squared_error(sparse_ratings.values, predictions)
   return loss
 
-class CFModel(object):
+class Collab_Filter_Model(object):
   """Simple class that represents a collaborative filtering model"""
   def __init__(self, embedding_vars, loss, metrics=None):
-    """Initializes a CFModel.
-    Args:
-      embedding_vars: A dictionary of tf.Variables.
-      loss: A float Tensor. The loss to optimize.
-      metrics: optional list of dictionaries of Tensors. The metrics in each
-        dictionary will be plotted in a separate figure during training.
-    """
-    self._embedding_vars = embedding_vars
-    self._loss = loss
-    self._metrics = metrics
+    
+    self.embedding_vars = embedding_vars
+    self.loss = loss
+    self.metrics = metrics
     self._embeddings = {k: None for k in embedding_vars}
-    self._session = None
+    self.session = None
 
   @property
   def embeddings(self):
@@ -131,62 +96,43 @@ class CFModel(object):
     return self._embeddings
         
 
-  def train(self, num_iterations=100, learning_rate=1.0, plot_results=False,
+  def train_data(self, number_of_iterations=100, lr=1.0, 
             optimizer=tf.train.GradientDescentOptimizer):
-    """Trains the model.
-    Args:
-      iterations: number of iterations to run.
-      learning_rate: optimizer learning rate.
-      plot_results: whether to plot the results at the end of training.
-      optimizer: the optimizer to use. Default to GradientDescentOptimizer.
-    Returns:
-      The metrics dictionary evaluated at the last iteration.
-    """
-    with self._loss.graph.as_default():
-      opt = optimizer(learning_rate)
-      train_op = opt.minimize(self._loss)
-      local_init_op = tf.group(
+    
+    with self.loss.graph.as_default():
+      opt = optimizer(lr)
+      training_optimizer = opt.minimize(self.loss)
+      initialise_local = tf.group(
           tf.variables_initializer(opt.variables()),
           tf.local_variables_initializer())
-      if self._session is None:
-        self._session = tf.Session()
-        with self._session.as_default():
-          self._session.run(tf.global_variables_initializer())
-          self._session.run(tf.tables_initializer())
+      if self.session is None:
+        self.session = tf.Session()
+        with self.session.as_default():
+          self.session.run(tf.global_variables_initializer())
+          self.session.run(tf.tables_initializer())
           tf.train.start_queue_runners()
 
-    with self._session.as_default():
-      local_init_op.run()
-      iterations = []
-      metrics = self._metrics or ({},)
-      metrics_vals = [collections.defaultdict(list) for _ in self._metrics]
+    with self.session.as_default():
+      initialise_local.run()
+      iterations_count = []
+      metrics = self.metrics or ({},)
+      metrics_vals = [collections.defaultdict(list) for _ in self.metrics]
 
-      # Train and append results.
-      for i in range(num_iterations + 1):
-        _, results = self._session.run((train_op, metrics))
-        if (i % 10 == 0) or i == num_iterations:
+     
+      for i in range(number_of_iterations + 1):
+        _, results = self.session.run((training_optimizer, metrics))
+        if (i % 10 == 0) or i == number_of_iterations:
           print("\r iteration %d: " % i + ", ".join(
                 ["%s=%f" % (k, v) for r in results for k, v in r.items()]),
                 end='')
-          iterations.append(i)
+          iterations_count.append(i)
           for metric_val, result in zip(metrics_vals, results):
             for k, v in result.items():
               metric_val[k].append(v)
 
-      for k, v in self._embedding_vars.items():
+      for k, v in self.embedding_vars.items():
         self._embeddings[k] = v.eval()
 
-      # if plot_results:
-      #   # Plot the metrics.
-      #   num_subplots = len(metrics)+1
-      #   fig = plt.figure()
-      #   fig.set_size_inches(num_subplots*10, 8)
-      #   for i, metric_vals in enumerate(metrics_vals):
-      #     ax = fig.add_subplot(1, num_subplots, i+1)
-      #     for k, v in metric_vals.items():
-      #       ax.plot(iterations, v, label=k)
-      #     ax.set_xlim([1, num_iterations])
-      #     ax.legend()
       return results
 
 
@@ -198,27 +144,19 @@ def gravity(U, V):
 def build_regularized_model(
     ratings, embedding_dim=3, regularization_coeff=.1, gravity_coeff=1.,
     init_stddev=0.1):
-  """
-  Args:
-    ratings: the DataFrame of book ratings.
-    embedding_dim: The dimension of the embedding space.
-    regularization_coeff: The regularization coefficient lambda.
-    gravity_coeff: The gravity regularization coefficient lambda_g.
-  Returns:
-    A CFModel object that uses a regularized loss.
-  """
-  # Split the ratings DataFrame into train and test.
-  train_ratings, test_ratings = split_dataframe(ratings)
+  
+  train_ratings, test_ratings = divide_train_test(ratings)
+  
   # SparseTensor representation of the train and test datasets.
-  A_train = build_rating_sparse_tensor(train_ratings)
-  A_test = build_rating_sparse_tensor(test_ratings)
+  A_train = get_matrix_from_ratings(train_ratings)
+  A_test = get_matrix_from_ratings(test_ratings)
   U = tf.Variable(tf.random_normal(
       [A_train.dense_shape[0], embedding_dim], stddev=init_stddev))
   V = tf.Variable(tf.random_normal(
       [A_train.dense_shape[1], embedding_dim], stddev=init_stddev))
 
-  error_train = sparse_mean_square_error(A_train, U, V)
-  error_test = sparse_mean_square_error(A_test, U, V)
+  error_train = calculate_mean_error(A_train, U, V)
+  error_test = calculate_mean_error(A_test, U, V)
   gravity_loss = gravity_coeff * gravity(U, V)
   regularization_loss = regularization_coeff * (
       tf.reduce_sum(U*U)/U.shape[0].value + tf.reduce_sum(V*V)/V.shape[0].value)
@@ -237,27 +175,18 @@ def build_regularized_model(
   print(losses)
   print(loss_components)
 
-  return CFModel(embeddings, total_loss, [losses, loss_components])
+  return Collab_Filter_Model(embeddings, total_loss, [losses, loss_components])
 
 
 reg_model = build_regularized_model(
     ratings, regularization_coeff=0.1, gravity_coeff=1.0, embedding_dim=35,
     init_stddev=.05)
-reg_model.train(num_iterations=20, learning_rate=20.)
+reg_model.train_data(number_of_iterations=2000, lr=20.)
 
 DOT = 'dot'
 COSINE = 'cosine'
 def compute_scores(query_embedding, item_embeddings, measure=DOT):
-  """Computes the scores of the candidates given a query.
-  Args:
-    query_embedding: a vector of shape [k], representing the query embedding.
-    item_embeddings: a matrix of shape [N, k], such that row i is the embedding
-      of item i.
-    measure: a string specifying the similarity measure to be used. Can be
-      either DOT or COSINE.
-  Returns:
-    scores: a vector of shape [N], such that scores[i] is the score of item i.
-  """
+  
   u = query_embedding
   V = item_embeddings
   if measure == COSINE:
@@ -283,12 +212,18 @@ def user_recommendations(model, measure=DOT, exclude_rated=False, k=6):
       df = df[df.book_id.apply(lambda book_id: book_id not in rated_books)]
     display.display(df.sort_values([score_key], ascending=False).head(k))  
 
-def book_neighbors(model, title_substring, measure=DOT, k=6):
+
+def book_neighbors(model, title_substring, measure=DOT, k=10):
   # Search for book ids that match the given substring.
-  ids =  books[books['title'].str.contains(title_substring)].index.values
+
+  title_substring = title_substring.lower()
+  title_substring = ''.join(e for e in title_substring if e.isalnum())
+
+  temp = books['edited_title'].str.contains(title_substring)
+  ids =  books[temp].index.values
   titles = books.iloc[ids]['title'].values
   if len(titles) == 0:
-    raise ValueError("Found no book with title %s" % title_substring)
+    raise ValueError("Found no books with title %s" % title_substring)
   print("Nearest neighbors of : %s." % titles[0])
   if len(titles) > 1:
     print("[Found more than one matching book. Other candidates: {}]".format(
@@ -301,7 +236,8 @@ def book_neighbors(model, title_substring, measure=DOT, k=6):
   df = pd.DataFrame({
       score_key: list(scores),
       'titles': books['title'],
-      'genres': books['all_genres']
+      'genres': books['all_genres'],
+      'image_url': books['img_url']
   })
   display.display(df.sort_values([score_key], ascending=False).head(k))
   return (df.sort_values([score_key], ascending=False).head(k))
@@ -309,8 +245,8 @@ def book_neighbors(model, title_substring, measure=DOT, k=6):
 
 user_recommendations(reg_model, DOT, exclude_rated=True, k=10)
 
-book_neighbors(reg_model, "The Kite Runner", DOT)
-book_neighbors(reg_model, "The Kite Runner", COSINE)
+# book_neighbors(reg_model, "Harry Potter", DOT)
+# book_neighbors(reg_model, "Harry Potter", COSINE)
 
 
 
@@ -322,6 +258,20 @@ def hello():
 
 @app.route('/predictions/<string:bookname>', methods=["GET"])
 def getPredictions(bookname):
-	pred1 = book_neighbors(reg_model, bookname, DOT)
-	pred2 = book_neighbors(reg_model, bookname, COSINE)
-	return (pred1.to_json(orient='index'))
+  # print("Rquesting recomendation for: " + bookname)
+  # bookname = bookname.lower()
+  # if not (bookname.isalnum()):
+  #   bookname = ''.join(e for e in bookname if e.isalnum())
+  # print("Bookname after formatting: " + bookname)
+
+  # df = books[books['edited_title'].astype(str).str.contains(bookname)]
+  # if df.empty == True:
+  #   new_dict ={}
+  #   new_dict['title'] = "No book in dataset"
+  #   return jsonify(new_dict)
+
+  # print("Found matching book with name: " + df.iloc[0]['title'])
+  pred1 = book_neighbors(reg_model, bookname, COSINE)
+  pred2 = book_neighbors(reg_model, bookname, DOT)
+
+  return (pred1.to_json(orient='index'))
